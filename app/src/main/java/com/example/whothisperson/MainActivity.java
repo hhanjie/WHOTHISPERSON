8153b0e90f49683e2b6ff5ff2f35af6264fa3b8c
hhanjie <37322239+hhanjie@users.noreply.github.com>
2019-10-26 15:16:16 -0700

    applied to use the camera function

--- app/src/main/java/com/example/whothisperson/MainActivity.java
+++ app/src/main/java/com/example/whothisperson/MainActivity.java
@@ -1,32 +1,45 @@
 package com.example.whothisperson;
 
 import android.Manifest;
-import android.content.pm.PackageManager;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.hardware.camera2.CameraManager;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    private Button takePictureButton;
     private ImageView imageView;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.imageView = (ImageView)this.findViewById(R.id.imageView);
+        Button photoButton = (Button) this.findViewById(R.id.shutterButton);
+        photoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+    }
 
-        //takePictureButton = (Button) findViewById(R.id.button_image);
-        //imageView = (ImageView) findViewById(R.id.imageview);
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) !=
-                PackageManager.PERMISSION_GRANTED) {
-            takePictureButton.setEnabled(false);
-            ActivityCompat.requestPermissions(this, new String[] {
-                    Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
         }
     }
 }
