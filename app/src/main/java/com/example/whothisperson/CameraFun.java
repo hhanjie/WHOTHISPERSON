3dd3b400a9a4052d014faf0479babbc22235147e
hhanjie <37322239+hhanjie@users.noreply.github.com>
2019-10-26 13:59:45 -0700

    save progress on Camera

---
+++ app/src/main/java/com/example/whothisperson/CameraFun.java
@@ -0,0 +1,45 @@
+package com.example.whothisperson;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Camera;
+import android.provider.MediaStore;
+import android.util.Log;
+
+
+public class CameraFun{
+    /*
+    Camera camera;
+    private boolean safeCameraOpen(int id) {
+        boolean qOpened = false;
+
+        try {
+            releaseCameraAndPreview();
+            camera = Camera.open(id);
+            qOpened = (camera != null);
+        } catch (Exception e) {
+            Log.e(getString(R.string.app_name), "failed to open Camera");
+            e.printStackTrace();
+        }
+
+        return qOpened;
+    }
+
+    private void releaseCameraAndPreview() {
+        preview.setCamera(null);
+        if (camera != null) {
+            camera.release();
+            camera = null;
+        }
+    }
+    */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open();
+        }
+        catch(Exception e){
+            //Camera not available
+        }
+        return c;
+    }
+}
--- app/src/main/java/com/example/whothisperson/MainActivity.java
+++ app/src/main/java/com/example/whothisperson/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.whothisperson;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.hardware.camera2.CameraManager;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button takePictureButton;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        takePictureButton = (Button) findViewById(R.id.button_image);
+        imageView = (ImageView) findViewById(R.id.imageview);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) !=
+                PackageManager.PERMISSION_GRANTED) {
+            takePictureButton.setEnabled(false);
+            ActivityCompat.requestPermissions(this, new String[] {
+                    Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
+        }
     }
 }
